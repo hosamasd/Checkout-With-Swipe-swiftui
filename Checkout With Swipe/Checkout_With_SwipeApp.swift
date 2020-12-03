aeb9b33c7c878636e5759d91f402c49203cf7668
hosam mohamed <hosamelmalt@gmail.com>
2020-12-03 13:22:09 -0800

    Initial Commit

---
+++ Checkout With Swipe/Checkout_With_SwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Checkout_With_SwipeApp.swift
+//  Checkout With Swipe
+//
+//  Created by Hossam on 12/3/20.
+//
+
+import SwiftUI
+
+@main
+struct Checkout_With_SwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
