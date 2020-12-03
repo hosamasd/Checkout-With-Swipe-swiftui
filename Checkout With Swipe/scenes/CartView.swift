fea1357cb331237b17948c96075ca667d11e30ca
hosam mohamed <hosamelmalt@gmail.com>
2020-12-03 13:57:31 -0800

    done

---
+++ Checkout With Swipe/scenes/CartView.swift
@@ -0,0 +1,20 @@
+//
+//  CartView.swift
+//  Checkout With Swipe
+//
+//  Created by Hossam on 12/3/20.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CartView_Previews: PreviewProvider {
+    static var previews: some View {
+        CartView()
+    }
+}
